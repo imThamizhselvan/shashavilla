f1fdd760f3eaf605470a9297fc649809fecc12eb
Thamizhselvan <thamizhtechie@gmail.com>
2018-12-05 20:02:29 +0530

    Adding initial layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import TopBar from './TopBar';
+import MenuBar from './MenuBar';
 import logo from './logo.svg';
 import './App.css';
+
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
+const AppRouter = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+          <li>
+            <Link to="/users/">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/about/" component={About} />
+      <Route path="/users/" component={Users} />
+    </div>
+  </Router>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TopBar />
+        <MenuBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Welcome to Shasha Villa
+            <AppRouter />
           </p>
         </header>
       </div>
---
+++ src/MenuBar/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MenuBar extends Component {
+  render() {
+    return (
+      <div>
+        <p> MenuBar bar </p>
+      </div>
+    );
+  }
+}
+
+export default MenuBar;
