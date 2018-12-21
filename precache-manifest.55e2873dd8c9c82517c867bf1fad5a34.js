f8b1fd13be084129f8beee6286e306cd9eb1c85b
Thamizhselvan <thamizhtechie@gmail.com>
2018-12-21 15:38:58 +0530

    Updates

---
+++ precache-manifest.55e2873dd8c9c82517c867bf1fad5a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3c954e90d99540fbc44281cc59a20e8",
+    "url": "/shashavilla/static/media/mist.c3c954e9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shashavilla/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4d8a85464b624f1bec7c",
+    "url": "/shashavilla/static/js/runtime~main.4d8a8546.js"
+  },
+  {
+    "revision": "1e8816126598ffced711",
+    "url": "/shashavilla/static/js/main.1e881612.chunk.js"
+  },
+  {
+    "revision": "4117c010560035cd866f",
+    "url": "/shashavilla/static/js/1.4117c010.chunk.js"
+  },
+  {
+    "revision": "1e8816126598ffced711",
+    "url": "/shashavilla/static/css/main.9fa9df6d.chunk.css"
+  },
+  {
+    "revision": "0427bd146fa7b31e3a479a5b0f36dab7",
+    "url": "/shashavilla/index.html"
+  }
+];
